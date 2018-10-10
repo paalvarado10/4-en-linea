a98c3bf1222ac79c539b307f1682876eb9d39281
paalavarado10 <pa.alvarado10@uniandes.edu.co>
2018-10-09 21:05:14 -0500

    pp

--- imports/api/records.js
+++ imports/api/records.js
@@ -15,19 +15,19 @@ Meteor.methods(
 {
 	"records.nuevoGanador":function(player1, player2, winner)
 	{
+    console.log("Lo esta Guardando: Player1: "+player1+", Player2: "+player2+", Winner: "+ winner);
     const r = Records.findOne({
       player1:player1,
       player2:player2,
       winner:winner
-    }, (r)=>{
-      console.log("ESTE ES EL RESULTADO DEL FIND ONE=> "+r+" <= PILAS");
-      if(r){
+    }, (res)=>{
+      console.log("ESTE ES EL RESULTADO DEL FIND ONE=> "+res+" <= PILAS");
+      if(res){
         console.log("Ya lo guardo");
         return "err: Ya lo guardo";
       }
       else
       {
-        console.log("Lo esta Guardando");
         Records.insert({
          player1:player1,
          player2:player2,
